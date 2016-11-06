0ebb2c9463a6f72c416a23e198943a3a22c2c02e
Stefano Gioia <stefanogch@gmail.com>
2016-11-06 19:08:24 -0300

    fixed directory & M

--- run.py
+++ run.py
@@ -5,14 +5,18 @@ import os
 
 cmd = []
 delta_time = 0.01
+directory = "default"
+M = 500
 
 for i in range(len(sys.argv)):
     if sys.argv[i] == "-M" and i + 1 < len(sys.argv):
         cmd = ["-M", sys.argv[i + 1]] + cmd
+        M = int(sys.argv[i + 1])
     elif sys.argv[i] == "-split" and i + 1 < len(sys.argv):
         cmd = ["-split", sys.argv[i + 1]] + cmd
     elif sys.argv[i] == "-d" and i + 1 < len(sys.argv):
         cmd = ["-d", sys.argv[i + 1]] + cmd
+        directory = sys.argv[i + 1]
     elif sys.argv[i] == "-sleep" and i + 1 < len(sys.argv):
         delta_time = float(sys.argv[i + 1])
     elif sys.argv[i] == "-test" and i + 1 < len(sys.argv):
