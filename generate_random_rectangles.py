16bb3da554875029a28a4847d2980a9cdc70a5d6
Sergio-P <elxsergio@gmail.com>
2016-11-06 19:42:58 -0300

    dataset random 5000 2

--- generate_random_rectangles.py
+++ generate_random_rectangles.py
@@ -5,7 +5,7 @@ N = int(sys.argv[1])
 
 min_value = 0
 max_value = 500000
-min_dim = 0
+min_dim = 1
 max_dim = 100
 
 buf = ""
@@ -13,8 +13,8 @@ buf = ""
 for i in range(N):
 	x = int(random.random()*(max_value - min_value))
 	y = int(random.random()*(max_value - min_value))
-	h = int(random.random()*(max_dim - min_dim))
-	w = int(random.random()*(max_dim - min_dim))
+	h = max(1,int(random.random()*(max_dim - min_dim)))
+	w = max(1,int(random.random()*(max_dim - min_dim)))
 	buf += "I " + str(x) + " " + str(y+h) + " " + str(x+w) + " " + str(y) + "\n"
 
 print buf
