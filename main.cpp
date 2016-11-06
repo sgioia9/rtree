99bebdd9dfdd70d4ee8fabceba5da4b4090c2312
Stefano Gioia <stefanogch@gmail.com>
2016-11-06 19:51:53 -0300

    skipping invalid rectangles:

--- main.cpp
+++ main.cpp
@@ -61,6 +61,10 @@ bool process_test_case() {
   RTree tree(m, M, split, directory);
   while (cin >> query_type >> x1 >> y1 >> x2 >> y2) {
     Rectangle rect(x1, y1, x2, y2);
+    if (x1 >= x2 || y2 >= y1) {
+      cout << "FOUND INVALID RECTANGLE << endl" << endl;
+      continue;
+    }
     if (query_type == 'I') tree.insert(rect);
     else if (query_type == 'F') tree.find(rect);
     else {
