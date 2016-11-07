5a126153bd68ee9c12525d5667ff41bd5e48c8ee
Sergio-P <elxsergio@gmail.com>
2016-11-07 10:55:52 -0300

    fixed iterator for find

--- find-iterator.py
+++ find-iterator.py
@@ -1,17 +1,13 @@
 import os
 import sys
 
-if len(sys.argv) < 2:
-    exit()
-
-split = sys.argv[1]
+splits = ["linear","greene"]
 
 for i in xrange(8, 23):
     N = 2**i
     print "Running " + str(N)
-    directory_name = split + "_2_to_the_" + str(i) + "th"
-    #os.system("rm -rf " + directory_name)
     file_name = "datasets/find_" + str(N) + ".txt"
-    #os.system("mkdir " + directory_name)
-    os.system("python generate_random_rectangles.py " + str(N) + " F > " + file_name)
-    os.system("python simple-run.py -split " + split + " -test " + file_name + " -M 57 -d " + directory_name + " > " + directory_name + "/find_results.txt")
+    os.system("python generate_random_rectangles.py " + str(N/10) + " F > " + file_name)
+    for split in splits:
+	    directory_name = split + "_2_to_the_" + str(i) + "th"
+	    os.system("python simple-run.py -split " + split + " -test " + file_name + " -M 57 -d " + directory_name + " > " + directory_name + "/find_results.txt")
